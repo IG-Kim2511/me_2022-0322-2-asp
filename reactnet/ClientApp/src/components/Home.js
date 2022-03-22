5d714e79625a4eef06d166a4081084a61c4e19d1
IG <iikim2511@gmail.com>
2022-03-22 10:41:19 -0400

    world

--- reactnet/ClientApp/src/components/Home.js
+++ reactnet/ClientApp/src/components/Home.js
@@ -7,6 +7,10 @@ export class Home extends Component {
     return (
       <div>
         <h1>Hello, Pioneering Programmers!</h1>
+        <main className='world'>
+          <div className='world-left'></div>
+          <div className='world-right'></div>
+        </main>
    
         <button className='myBtn'>btn</button>
       </div>
