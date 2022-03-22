33d489e0132636429b45d2ba1f8976c2b93a72fe
IG <iikim2511@gmail.com>
2022-03-22 12:14:23 -0400

    input text

--- reactnet/ClientApp/src/App.js
+++ reactnet/ClientApp/src/App.js
@@ -3,30 +3,48 @@ import './App.css'
 
 
 function App() {
-  const [theme, setTheme] = useState("")
   const [data, setData] = useState("")
+  
+  // {/* js 1149 */}  {/* js 1208 */}
+  const [input, setInput] = useState("")
   
   return (
     <div>
-    <div className='title'>
-      <h1>Hello, Pioneering Programmers!</h1>
+      <div className='title'>
+        <h1>Hello, Pioneering Programmers!</h1>
       </div>
-      <main className='world'>
-        
-        <div className='world-left'>
-          <button className='myBtn' onClick={()=>{setData('Theme was set to Dark')}} >dark theme</button>
-          <button className='myBtn btn_me' onClick={()=>{setData('Button 3 was added')}}>white</button>
-          <div className="text">text</div>
-        
-        </div>
-        <div className='world-right'>
+        <main className='world'>
           
-          <div dangerouslySetInnerHTML={{__html: theme}}/>
-          <div dangerouslySetInnerHTML={{__html: data}}/>
-        </div>
-      </main>
+          <div className='world-left'>
+            <button className='myBtn' onClick={()=>{setData('Theme was set to Dark')}} >dark theme</button>
+            <button className='myBtn btn_me' onClick={()=>{setData('Button 3 was added')}}>white</button>
+            <div className="text">text</div>
+          
+          </div>
+          <div className='world-right'>
+            
+            <div dangerouslySetInnerHTML={{__html: data}}/>
+            
+            {/* js 1149 */}
+            {/* <div>“Message Sent: [{input} from text area]”</div> */}
+            
+          </div>
+        </main>
 
-  </div>
+        <footer>
+          {/* js 1149 */}
+          <input type="text" value={input}  onChange={(e) => setInput(e.target.value)} />
+
+
+
+          {/* 🍀js 1208. Sending input text when I click button */}
+          <button className='myBtn' onClick={()=>{setData(`“Message Sent: [${input} from text area]”`)}}>send</button>
+
+
+
+          <button className='myBtn clear' onClick={()=>{setData(``)}}>clear</button>
+        </footer>
+    </div>
   );
 }
 
