07b6c7677af9212383cad0e43c15113873c8574e
IG <iikim2511@gmail.com>
2022-03-22 18:08:47 -0400

    createBtnMessage

--- reactnet/ClientApp/src/App.js
+++ reactnet/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
   
+  // 🍀{/* js 220540 */}  
   const [data, setData] = useState("")
   
   // {/* js 1149 */}  {/* js 1208 */}
@@ -87,37 +88,31 @@ function App() {
 
   
     
-  // 🍀 {/* js 1208, js 1149 */}
-  function submit() {
-    setData(`“Message Sent: [${input} from text area]”`)
-    
-    setInput("");   
-  }
 
-    //  {/* 🍀js 0108 */}  
+
+    //  {/* 🍀js 0108. create button  */}  
   const renderingBtnElm = document.querySelector('.renderingBtn');
 
   const [i, setI] = useState(1)
 
   function createBtn() {
-  
- 
+
       let button = document.createElement('button');
       button.classList.add(`myBtn`)
-
   
       setI(i+1);
       button.innerHTML = `button ${i}`;
   
       button.onclick = function(){
-           setData(`${dateTime} Button ${i} clicked`)
+          //  setData(`${dateTime} Button ${i} clicked`)
+          createBtnMessage(`clicked`)
               return false;
           };
       
       renderingBtnElm.appendChild(button);
 
-
-      setData(`${dateTime} button ${i} added`)
+      // setData(`${dateTime} button ${i} added`)
+      createBtnMessage(`added`)
   }
 
 
@@ -125,9 +120,44 @@ function App() {
   const renderingBtn = document.querySelector('.renderingBtn');
 
   function clear() {
-    setData(``)
+    // setData(``)
+    renderingMessageE.innerHTML=``;
     renderingBtn.innerHTML=``
     setI(1);  
+  }
+  
+
+
+  // 🍀 {/* js 220540 */}  
+
+  const renderingMessageE = document.querySelector('.renderingMessage');
+
+  function createBtnMessage(p_message) {
+      let div = document.createElement('div');
+      div.classList.add(`text`)
+  
+      div.innerHTML = `${dateTime} button ${i} ${p_message}`;
+        
+      renderingMessageE.appendChild(div);
+  }
+
+
+function createMessage(p_message) {
+  let div = document.createElement('div');
+  div.classList.add(`text`)
+
+  div.innerHTML = `${p_message}`;
+    
+  renderingMessageE.appendChild(div);
+  
+}
+
+  // 🍀 {/* js 1208, js 1149 */}
+  function submit() {
+
+     createMessage(`“Message Sent: [${input} from text area]”`);
+
+     setInput("");   
   }
 
 
@@ -143,7 +173,6 @@ function App() {
 
             <button className='myBtn light theme' onClick={()=>{ dark ==="dark" ? darkMode() : lightMode()}}>{dark} theme</button>
 
-            <div className="text light">text</div>
 
             {/* js 0108 */}
             <div className='renderingBtn'></div>
@@ -153,10 +182,13 @@ function App() {
 
           <section className='world-right light'>
             
+            {/* js 220540 */}
             <div dangerouslySetInnerHTML={{__html: data}}/>
 
-            
-            <div className="text light">text</div>
+
+            {/* js 220540 */}  
+            <div className='renderingMessage'></div>
+
             
             {/* js 1149 */}
             {/* <div>“Message Sent: [{input} from text area]”</div> */}
